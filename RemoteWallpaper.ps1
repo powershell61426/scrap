8567c59f5744cd5ed3d54e7a9f52ee02cb04b0e1
powershell61426 <peipeiyu00@gmail.com>
2026-06-15 23:20:28 -0400

    Add files via upload

---
+++ RemoteWallpaper.ps1
@@ -0,0 +1,8 @@
+﻿New-PSSession -ComputerName PCName -Credential $cred
+Copy-Item "C:\LocalDestination\background.jpg" -Destination "C:\RemoteDestination\background.jpg" -ToSession(Get-PSSession -Id X)
+Invoke-Command -Session(Get-PSSession -Id X) -ScriptBlock{ 
+    Set-ItemProperty` 
+    -Path "HKCU:\Control Panel\Desktop"` 
+    -Name Wallpaper` 
+    -Value "C:\LocalDestination\background.jpg"`
+}
---
+++ WinRM.ps1
@@ -0,0 +1,3 @@
+﻿Start-Service -name WinRM
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value PCName -Force
+Enable-PSRemoting -SkipNetworkProfileCheck -Force
