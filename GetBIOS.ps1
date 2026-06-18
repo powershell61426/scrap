7c08cb4ce3375a74bed3a36624ed9af0a61af951
powershell61426 <peipeiyu00@gmail.com>
2026-06-18 17:48:59 -0400

    Add files via upload

---
+++ GetBIOS.ps1
@@ -0,0 +1,4 @@
+﻿$s = New-PSSession -ComputerName paopaopc -Credential $cred
+Invoke-Command -Session $s -ScriptBlock {function Get-Bios {Get-WmiObject Win32_Bios}}
+Invoke-Command -Session $s -ScriptBlock {Get-Bios}
+Import-PSSession -Session $s -CommandName Get-Bios
