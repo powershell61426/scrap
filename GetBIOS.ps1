d7975208e8e0844028d86732f93ccbafa22b4912
powershell61426 <peipeiyu00@gmail.com>
2026-06-23 18:38:57 -0400

    Delete GetBIOS.ps1

--- GetBIOS.ps1
+++
@@ -1,4 +0,0 @@
-﻿$s = New-PSSession -ComputerName paopaopc -Credential $cred
-Invoke-Command -Session $s -ScriptBlock {function Get-Bios {Get-WmiObject Win32_Bios}}
-Invoke-Command -Session $s -ScriptBlock {Get-Bios}
-Import-PSSession -Session $s -CommandName Get-Bios
