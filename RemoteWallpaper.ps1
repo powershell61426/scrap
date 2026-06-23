23e4c5fa4243bb46a4c02883ea1f3104538d4ef2
powershell61426 <peipeiyu00@gmail.com>
2026-06-23 18:39:27 -0400

    Delete RemoteWallpaper.ps1

--- RemoteWallpaper.ps1
+++
@@ -1,8 +0,0 @@
-﻿New-PSSession -ComputerName PCName -Credential $cred
-Copy-Item "C:\LocalDestination\background.jpg" -Destination "C:\RemoteDestination\background.jpg" -ToSession(Get-PSSession -Id X)
-Invoke-Command -Session(Get-PSSession -Id X) -ScriptBlock{ 
-    Set-ItemProperty` 
-    -Path "HKCU:\Control Panel\Desktop"` 
-    -Name Wallpaper` 
-    -Value "C:\RemoteDestination\background.jpg"`
-}
